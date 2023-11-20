47730620cdbe7d3ae4499104e6ee295550b953fa
Takuma Kira <takuma@kirakiraworx.com>
2023-11-20 18:42:35 +0900

    [dev] Define NoteRepository trait

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -1,3 +1,5 @@
+use anyhow::Result;
+use async_trait::async_trait;
 use sqlx::FromRow;
 
 #[derive(Debug, thiserror::Error)]
@@ -15,4 +17,13 @@ pub struct Note {
     pub title: String,
     pub content: String,
     pub created_at: String,
+}
+
+#[async_trait]
+pub trait NoteRepository {
+    async fn all(&self) -> Result<Vec<Note>>;
+    async fn get(&self, id: &str) -> Result<Note>;
+    async fn create(&self, note: &NewNote) -> Result<Note>;
+    async fn update(&self, id: &str, note: &UpdateNote) -> Result<Note>;
+    async fn delete(&self, id: &str) -> Result<Note>;
 }
