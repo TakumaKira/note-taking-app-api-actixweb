f731a15005a442ed59f19983141abdaf9fe22725
Takuma Kira <takuma@kirakiraworx.com>
2023-11-20 20:01:52 +0900

    [dev] Define NoteService

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use anyhow::Result;
+use async_trait::async_trait;
+use db::NewNote;
+use db::Note;
+use db::UpdateNote
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[async_trait]
+pub trait NoteService: Sync + Send {
+    async fn all(&self) -> Result<Vec<Note>>;
+    async fn get(&self, id: &str) -> Result<Note>;
+    async fn create(&self, note: &NewNote) -> Result<Note>;
+    async fn update(&self, id: &str, note: &UpdateNote) -> Result<Note>;
+    async fn delete(&self, id: &str) -> Result<Note>;
 }
