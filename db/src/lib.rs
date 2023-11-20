c58a104bc28173177002ffe5035a6d0cbd1b355c
Takuma Kira <takuma@kirakiraworx.com>
2023-11-20 18:31:56 +0900

    [dev] Define Note struct

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -1,3 +1,5 @@
+use sqlx::FromRow;
+
 #[derive(Debug, thiserror::Error)]
 pub enum DbError {
     #[error("NotFound")]
@@ -5,4 +7,12 @@ pub enum DbError {
 
     #[error(transparent)]
     DatabaseError(#[from] sqlx::Error)
+}
+
+#[derive(Debug, FromRow, PartialEq, Eq)]
+pub struct Note {
+    pub id: String,
+    pub title: String,
+    pub content: String,
+    pub created_at: String,
 }
