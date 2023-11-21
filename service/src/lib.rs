dd1c808454050f13a083a2bb5f6f9f2be7bb16fa
Takuma Kira <takuma@kirakiraworx.com>
2023-11-21 10:57:55 +0900

    [dev] Implement NoteServiceImpl

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use async_trait::async_trait;
 use db::NewNote;
 use db::Note;
+use db::NoteRepository;
 use db::UpdateNote
 
 #[async_trait]
@@ -11,4 +12,14 @@ pub trait NoteService: Sync + Send {
     async fn create(&self, note: &NewNote) -> Result<Note>;
     async fn update(&self, id: &str, note: &UpdateNote) -> Result<Note>;
     async fn delete(&self, id: &str) -> Result<Note>;
+}
+
+pub struct NoteServiceImpl<R: NoteRepository + Send + Sync> {
+    repository: R,
+}
+
+impl<R: NoteRepository + Send + Sync> NoteServiceImpl<R> {
+    pub fn new(repository: R) -> Self {
+        NoteServiceImpl { repository }
+    }
 }
