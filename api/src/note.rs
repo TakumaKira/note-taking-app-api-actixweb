1bc5e11f6766e3864ef726a2fc9341e276aeb495
Takuma Kira <takuma@kirakiraworx.com>
2023-11-25 16:04:50 +0900

    [dev] Write tests for api layer

--- api/src/note.rs
+++ api/src/note.rs
@@ -8,7 +8,7 @@ use uuid::Uuid;
 use crate::error::ApiError;
 use crate::domain::{ErrorResponse, MessageResponse};
 #[cfg(test)]
-use mockall::{mock, predicate::*}
+use mockall::{mock, predicate::*};
 
 pub(super) fn configure(note_service: Data<Box<dyn NoteService>>) -> impl FnOnce(&mut ServiceConfig) {
   |config: &mut ServiceConfig| {
@@ -133,7 +133,7 @@ pub(super) async fn create_note(note_service: Data<Box<dyn NoteService>>, create
     title: create_note.title.clone(),
     content: create_note.content.clone(),
     created_at: chrono::offset::Utc::now().native_utc().to_string(),
-  }
+  };
   let db_note = note_service.create(&new_note).await?;
   let api_note = Note::from(db_note);
 
@@ -201,5 +201,190 @@ mod tests {
       async fn update(&self, id: &str, note: &db::UpdateNote) -> Result<db::Note>;
       async fn delete(&self, id: &str) -> Result<db::Note>;
     }
+  }
+
+  #[actix_web::test]
+  async fn test_list_notes() {
+    let mut mock_service = MockService::new();
+
+    mock_service.expect_all()
+      .times(1)
+      .returning(|| Ok(vec![db::Note {
+        id: String::from("14322988-32fe-447c-ac38-06fb6c699b4a"),
+        title: String::from("Note 1"),
+        content: String::from("This is note #1."),
+        created_at: String::from("2021-01-01T00:00:00Z"),
+      }]));
+
+    let note_service_data = Data::new(Box::new(mock_service) as Box<dyn NoteService>);
+
+    let mut app = test::init_service(
+      App::new().configure(configure(note_service_data.clone()))
+    ).await;
+
+    let req = test::TestRequest::get().uri("/notes").to_request();
+    let resp = test::call_service(&mut app, req).await;
+
+    assert!(resp.status().is_success());
+  }
+
+  // #[actix_web::test]
+  // async fn test_get_note() {
+
+  // }
+
+  #[actix_web::test]
+  async fn test_create_note() {
+    let mut mock_service = MockService::new();
+
+    let new_note = db::NewNote {
+      id: String::from("new-id"),
+      title: String::from("Note 1"),
+      content: String::from("This is note #1."),
+      created_at: String::from("2021-01-01T00:00:00Z"),
+    };
+    let new_note_test = new_note.clone();
+    mock_service.expect_create()
+      .times(1)
+      .returning(move |_| Ok(db::Note {
+        id: String::from("new-id"),
+        title: new_student_test.title.clone(),
+        content: new_student_test.content.clone(),
+        created_at: new_student_test.created_at.clone(),
+      }));
+
+    let note_service_data = Data::new(Box::new(mock_service) as Box<dyn NoteService>);
+
+    let mut app = test::init_service(
+      App::new().configure(configure(note_service_data.clone()))
+    ).await;
+
+    let note = CreateNoteRequest {
+      title: "Note 1".to_string(),
+      content: "This is note #1.".to_string(),
+    };
+
+    let req = test::TestRequest::post()
+      .uri("/notes")
+      .set_json(&note)
+      .to_request();
+
+    let resp = test::call_service(&mut app, req).await;
+
+    assert!(resp.status().is_success());
+
+    let body = test::read_body(resp).await;
+
+    let returned_note: CreateNoteResponse = serde_json::from_slice(&body).unwrap();
+
+    let expected_note = db::Note {
+      id: String::from("new-id"),
+      title: String::from("Note 1"),
+      content: String::from("This is note #1."),
+      created_at: String::from("2021-01-01T00:00:00Z"),
+    };
+    assert_eq!(returned_note.note.id, expected_note.id);
+    assert_eq!(returned_note.note.title, expected_note.title);
+    assert_eq!(returned_note.note.content, expected_note.content);
+    assert_eq!(returned_note.note.created_at, expected_note.created_at);
+  }
+
+  #[actix_web::test]
+  async fn test_update_note() {
+    let mut mock_service = MockService::new();
+
+    let note_id = "some-id";
+    let update_request = UpdateNoteRequest {
+      title: "Updated Title".to_string(),
+      content: "Updated content".to_string(),
+    };
+    let updated_note = db::UpdateNote {
+      title: update_request.title.clone(),
+      content: update_request.content.clone(),
+    };
+    let updated_note_test = updated_note.clone();
+    let updated_note_test_predicate = updated_note.clone();
+    mock_service.expect_update()
+      .with(predicate::eq(note_id), predicate::eq(updated_note_test_predicate))
+      .times(1)
+      .returning(move |_, _| Ok(db::Note {
+        id: note_id.clone().to_string(),
+        title: updated_note_test.title.clone(),
+        content: updated_note_test.content.clone(),
+        created_at: String::from("2021-01-01T00:00:00Z"),
+      }));
+
+    let note_service_data = Data::new(Box::new(mock_service) as Box<dyn NoteService>);
+
+    let mut app = test::init_service(
+      App::new().configure(configure(note_service_data.clone()))
+    ).await;
+
+    let req = test::TestRequest::put()
+      .uri(&format!("/notes/{}", note_id))
+      .set_json(&update_request)
+      .to_request();
+
+    let resp = test::call_service(&mut app, req).await;
+
+    assert!(resp.status().is_success());
+
+    let body = test::read_body(resp).await;
+    let returned_Note: UpdateNoteResponse = serde_json::from_slice(&body).unwrap();
+
+    assert_eq!(returned_note.Note.title, update_request.title);
+    assert_eq!(returned_note.Note.content, update_request.content);
+  }
+
+  #[actix_web::test]
+  async fn test_delete_note() {
+    let mut mock_service = MockService::new();
+
+    let note_id = "some-id";
+    mock_service.expect_delete()
+      .with(predicate::eq(note_id))
+      .times(1)
+      .returning(|_| Ok(db::Note {
+        id: note_id.to_string(),
+        title: String::from("Note 1"),
+        content: String::from("This is note #1."),
+        created_at: String::from("2021-01-01T00:00:00Z"),
+      }));
+
+    let note_service_data = Data::new(Box::new(mock_service) as Box<dyn NoteService>);
+
+    let mut app = test::init_service(
+      App::new().configure(configure(note_service_data.clone()))
+    ).await;
+
+    let req = test::TestRequest::delete()
+      .uri(&format!("/notes/{}", note_id))
+      .to_request();
+
+    let resp = test::call_service(&mut app, req).await;
+
+    assert!(resp.status().is_success());
+    assert_eq!(resp.status(), 204);
+  }
+
+  #[actix_web::test]
+  async fn test_delete_student_not_found() {
+      let mut mock_service = MockService::new();
+
+      // Set up the mock to return an error of type db::NotFound
+      mock_service.expect_delete()
+          .times(1)
+          .returning(|_| Err(anyhow::anyhow!(db::DbError::NotFound)));
+
+      let student_service_data = Data::new(Box::new(mock_service) as Box<dyn StudentService>);
+
+      let mut app = test::init_service(
+          App::new().configure(configure(student_service_data.clone()))
+      ).await;
+
+      let req = test::TestRequest::delete().uri("/students/some_id").to_request();
+      let resp = test::call_service(&mut app, req).await;
+
+      assert_eq!(resp.status(), 404);
   }
 }
